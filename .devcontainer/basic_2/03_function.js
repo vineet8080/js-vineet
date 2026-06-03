49aa985126952694d5a69331d206aa6d79067b83
vineet8080 <vineet2020h@gmail.com>
2026-06-03 07:07:23 +0000

    funct&scope

---
+++ .devcontainer/basic_2/03_function.js
@@ -0,0 +1,21 @@
+function mybook(){
+    console.log("vineet");
+    console.log("vineet");
+     console.log("vineet");
+      console.log("vineet"); 
+}
+
+
+// mybook()
+// mybook()
+
+
+function addition (num1,num2){
+    return num1+num2
+
+}
+const result=addition(3,5)
+console.log(result)
+
+
+
---
+++ .devcontainer/basic_2/04_scope.js
@@ -0,0 +1,18 @@
+// let a=10
+// const b=90
+// var c=97
+ 
+if(true){
+   let a=10
+   const b=90
+   var c=97   //avoid var that why
+
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//scope ke ander hai wo bahar nhi jana chahiye lekin jo global hai wo ander aa sakta hai
