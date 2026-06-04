c9f602c091965c8cc532378f301aaa11f1bfbd3e
vineet8080 <vineet2020h@gmail.com>
2026-06-04 08:46:57 +0000

    centrolflow

---
+++ .devcontainer/controlflow_3/switch.js
@@ -0,0 +1,56 @@
+switch (key) {
+    case value:       //syntax switch case
+        
+        break;
+
+    default:
+        break;
+}
+
+
+let month="jan"
+
+switch (month) {
+    case "jan":
+        console.log("jan")
+        break;
+    case "Feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break; 
+
+    default:
+        console.log("invalid data ");
+        
+        break;
+}
+
+
+let month=5
+
+switch (month) {
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break; 
+
+    default:
+        console.log("invalid data ");
+        
+        break;
+}
+
+
---
+++ .devcontainer/controlflow_3/truthy.js
@@ -0,0 +1,14 @@
+const vineet = []
+
+if (vineet) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
