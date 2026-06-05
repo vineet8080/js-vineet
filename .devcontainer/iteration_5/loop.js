ba1356c54010d09f5d5dbd193ff0dd178a65fabc
vineet8080 <vineet2020h@gmail.com>
2026-06-05 07:04:57 +0000

    loop

---
+++ .devcontainer/iteration_5/loop.js
@@ -0,0 +1,62 @@
+//loops 
+
+//1.for loop
+
+// for (let i = 0; i <10; i++) {
+//     const element = i;
+//     console.log(i); 
+// }
+
+
+// for (let i = 1; i <10; i++) {
+//     console.log(`outer loop: ${i}`);   
+//     for (let j = 1; j<= 10; j++) {
+//         // console.log(`inner loop : ${j}`);
+
+//         console.log(`${i}x${j}=${i*j}`);
+            
+//     } 
+// }
+
+
+// for aaray
+// let array=[1,2,3,4,5,6]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+    
+// }
+// let array=["vineet","yadav","name","ram"]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+    
+// }
+
+
+
+// break 
+
+// for (let i = 0; i <10; i++) {
+  
+//     if (i==5) {
+//         console.log("5 is found");
+//         break
+        
+//     }
+//     console.log(i);
+    
+// }
+
+//break
+
+// for (let i = 0; i <10; i++) {
+  
+//     if (i==5) {
+//         console.log("5 is found");
+//         continue
+        
+//     }
+//     console.log(i);
+    
+// }
